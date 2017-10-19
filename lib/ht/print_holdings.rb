0a4d15231f6d41bfbce38d89e69d4a1531438a6c
Bill Dueber <bill@dueber.com>
2017-10-19 17:23:12 -0400

    Can't use prepared stmts with an array bound variable with JDBC

--- lib/ht/print_holdings.rb
+++ lib/ht/print_holdings.rb
@@ -6,8 +6,7 @@ module HT
 
     PHDB_Query = HT::DB.instance[:holdings_htitem_htmember]
                    .select(:volume_id, :member_id)
-                   .where(volume_id: :$volume_ids)
-                   .prepare(:select, :print_holdings_for_htids)
+
 
     def initialize(passed_seeds = [])
       seeds     = Array(passed_seeds)
@@ -23,7 +22,7 @@ module HT
       if @htid_map.size > 1000
         @htid_map = empty_map
       end
-      PHDB_Query.call(volume_ids: Array(htid_seeds)).each do |r|
+      PHDB_Query.where(volume_ids: Array(htid_seeds)).each do |r|
         @htid_map[r[:volume_id]] << r[:member_id]
       end
     end
