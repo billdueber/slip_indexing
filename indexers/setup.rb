513dc6a54d9e88e80c3e3806ce68fb6d2cddb230
Bill Dueber <bill@dueber.com>
2017-10-19 22:56:03 -0400

    Lambda-creator for simple mappings

--- indexers/setup.rb
+++ indexers/setup.rb
@@ -1,4 +1,17 @@
-thisdir = __dir__
+$:.unshift File.absolute_path File.join(__dir__, '..', 'lib')
 
-$:.unshift File.join(thisdir, '..', 'lib')
 
+
+# Utility functions
+#
+# A simple way to construct a lambda that pulls the right thing out of
+# the catalog record and sticks it in the accumulator
+#
+# Man. Really should rewrite traject to not have so many damn side effects!
+
+def catalog_metadata_extractor(catfield, catalog_fieldname_map)
+  ->(item, acc) do
+    key = catalog_fieldname_map[catfield]
+    acc << item.catalog_metadata[key]
+  end
+end
